dc0b1c573368b1eb29f2e370121dda3e91d78132
puripuri2100 <puripuri2100@gmail.com>
2024-05-09 22:31:05 +0900

    update README

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
-//! [法令標準XMLスキーマ](https://elaws.e-gov.go.jp/file/XMLSchemaForJapaneseLaw_v3.xsd)で定義された規格に基づいてXMLとの相互変換を行う
+//! [![crates.io][crates-badge]][crates]
+//! [![Build Status][ci-badge]][ci]
+//! 
+//! [crates]: https://crates.io/crates/japanese_law_xml_schema
+//! [crates-badge]: https://img.shields.io/crates/v/japanese_law_xml_schema
+//! [ci]: https://github.com/japanese-law-analysis/japanese_law_xml_schema/actions/workflows/ci.yaml
+//! [ci-badge]: https://github.com/japanese-law-analysis/japanese_law_xml_schema/actions/workflows/ci.yaml/badge.svg
+//! 
+//! # 概要
+//! [法令標準XMLスキーマ](https://elaws.e-gov.go.jp/file/XMLSchemaForJapaneseLaw_v3.xsd)で定義された規格に基づいてXMLとの相互変換を行うライブラリです。
+//! 
+//! テキストを解析して構造を構築することもできます。
 //!
+//! --- 
+//! 
+//! [The MIT License](https://github.com/japanese-law-analysis/japanese_law_xml_schema/blob/master/LICENSE)
+//! 
+//! (c) 2024 Naoki Kaneko (a.k.a. "puripuri2100")
 //!
 
 #![recursion_limit = "256"]
